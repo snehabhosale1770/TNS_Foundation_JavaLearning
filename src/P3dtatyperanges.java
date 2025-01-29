a8a5cd36cff47ac6c0fa690693369cef27185126
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-29 15:01:38 +0530

    datatype range program

---
+++ src/P3dtatyperanges.java
@@ -0,0 +1,24 @@
+public class P3dtatyperanges {
+    public static void main(String[] args) {
+
+         byte byteMax = 127;
+         byte byteMin = -128;
+        System.out.println("Max Range of byte is : " + byteMax);
+        System.out.println("Min Range of byte is : " + byteMin);
+
+        short shortMax = 32767;
+        short shortMin = -32768;
+        System.out.println("Max Range of short is : " + shortMax);
+        System.out.println("Min Range of short is : " + shortMin);
+
+        int intMax = 214748364;
+        int intMin = -2147483368;
+        System.out.println("Max Range of int is : " + intMax);
+        System.out.println("Min Range of int is : " + intMin);
+
+        long longMax = 922337203685477580L;
+        long longMin = -9223372036854775808L;
+        System.out.println("Max Range of long is : " + longMax);
+        System.out.println("Min Range of long is : " + longMin);
+    }
+}
