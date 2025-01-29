d4581fe4adaa0fc8ad480b526d85a7521fdd14aa
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-29 14:28:48 +0530

    variable program initialization and declaration

---
+++ src/P1variable.java
@@ -0,0 +1,11 @@
+public class P1variable {
+    public static void main(String[] args) {
+        int age = 21; // variable initialization
+        System.out.println("Your age is : " + age);
+
+        String name; // variable declaration
+        name = "Sneha";
+        System.out.println("Your Name is " + name);
+
+    }
+}
