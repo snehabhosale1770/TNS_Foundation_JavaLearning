1dde5a4af351b616e936e937b97022c93b95597e
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-30 21:15:20 +0530

    P6 If Else program

--- src/P6IfElseProgram.java
+++ src/P6IfElseProgram.java
@@ -1,7 +1,7 @@
 public class P6IfElseProgram {
     public static void main(String[] args) {
         int age = 21;
-        if(age > 18){
+        if(age >= 18){
             System.out.println("Your are eligible for voting");
         }
         else{
