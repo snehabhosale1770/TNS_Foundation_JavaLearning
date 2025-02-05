354f355b5e0975dbcf15d49de610df3157ec64d8
snehabhosale1770 <snehabhosale683@gmail.com>
2025-02-05 14:17:43 +0530

    P If Decision making without operator

--- src/P6IfElseProgram.java
+++ src/P6IfElseProgram.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class P6IfElseProgram {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int age = 21;
         if(age >= 18){
             System.out.println("Your are eligible for voting");
