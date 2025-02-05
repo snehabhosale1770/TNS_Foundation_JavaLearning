9bbe00420a5d3b4510743ac18cbc5388d58e0d9a
snehabhosale1770 <snehabhosale683@gmail.com>
2025-02-05 14:24:23 +0530

    P8 Nested if else using operator

---
+++ src/P8nestedIfElse.java
@@ -0,0 +1,16 @@
+public class P8nestedIfElse {
+    public static void main(String[] args) {
+
+        int a=10, b=20,c=5;
+        if((a>b) && (a>c)){
+            System.out.println("a is greater number");
+        }
+        else if ((b>a) && (b>c)){
+            System.out.println("b is greater number");
+        }
+          else {
+            System.out.println("c is greater number");
+        }
+        }
+    }
+
