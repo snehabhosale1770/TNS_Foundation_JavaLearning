281a4661849bbb234808b59c658c871c0ebf0147
snehabhosale1770 <snehabhosale683@gmail.com>
2025-02-05 14:40:37 +0530

    P10 P11 P12 for, while, do while loop

---
+++ src/P11whileLoop.java
@@ -0,0 +1,10 @@
+public class P11whileLoop {
+    public static void main(String[] args) {
+
+        int i = 95;
+        while(i <= 100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
