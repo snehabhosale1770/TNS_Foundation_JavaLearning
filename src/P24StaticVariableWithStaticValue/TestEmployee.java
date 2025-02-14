7794e4714621b11dfe22ab2d6f677f36dcdb78c8
snehabhosale1770 <snehabhosale683@gmail.com>
2025-02-14 15:12:54 +0530

    P23 Static Variable With Static Value program

---
+++ src/P24StaticVariableWithStaticValue/TestEmployee.java
@@ -0,0 +1,10 @@
+package P24StaticVariableWithStaticValue;
+
+public class TestEmployee {
+
+    public static void main(String[] args) {
+        Employee e=new Employee("Sneha",1);
+        System.out.println(e);
+}
+
+}
