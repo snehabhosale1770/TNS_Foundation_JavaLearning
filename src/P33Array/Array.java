1527726966446591976a8578ac60c3ebe4c9e73e
snehabhosale1770 <snehabhosale683@gmail.com>
2025-03-05 12:10:41 +0530

    P33 Array program

---
+++ src/P33Array/Array.java
@@ -0,0 +1,12 @@
+package P33Array;
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int intArr[]={4,5,6,7,8};
+        System.out.println(Arrays.toString(intArr));
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.println(intArr[i]+" ");
+    }
+    }
+}
